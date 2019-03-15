e355802835728acaca8807d3554da2e6f25a19a6
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-16 00:23:47 +0200

    Finish 1.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom';
 
 const Header = ({ title }) => <h1>{title}</h1>;
 const Total = ({ total }) => <p>Yhteensä {total} tehtävää</p>;
-const Part = ({ name, quantity }) => <p>{name} {quantity}</p>;
-const Content = ({ parts }) => parts.map(({ name, quantity }, i) =>
-    <Part key={i} name={name} quantity={quantity} />);
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>;
+const Content = ({ parts }) => parts.map(({ name, exercises }, i) =>
+    <Part key={i} name={name} exercises={exercises} />);
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys';
-    const part1 = 'Reactin perusteet';
-    const exercises1 = 10;
-    const part2 = 'Tiedonvälitys propseilla';
-    const exercises2 = 7;
-    const part3 = 'Komponenttien tila';
-    const exercises3 = 14;
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10,
+    };
+    const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+    };
+    const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14,
+    };
 
     return (
         <div>
             <Header title={course} />
-            <Content parts={[
-                { name: part1, quantity: exercises1 },
-                { name: part2, quantity: exercises2 },
-                { name: part3, quantity: exercises3 }
-            ]} />
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Content parts={[part1, part2, part3]} />
+            <Total total={part1.exercises + part2.exercises + part3.exercises} />
         </div>
     );
 }
