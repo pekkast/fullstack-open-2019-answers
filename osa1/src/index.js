eb54a63e0723887eb73b8b562fd6c0feed960840
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-15 23:43:57 +0200

    Add props - spreaded

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Hello = () => (
+const Hello = ({name, age}) => (
     <div>
-        <p>Hello world</p>
+        <p>Hello {name}, you are {age} years</p>
     </div>
 );
 
 const App = () => {
+    const young = 17;
     return (
         <div>
-            <Hello />
-            <Hello />
-            <Hello />
+            <Hello name="Jussi" age={young} />
+            <Hello name="Matti" age={young + 7} />
+            <Hello name="Pekka" age={99} />
         </div>
     );
 }
