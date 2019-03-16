f1b9da1fbc2d75e1db42182c5c995131cff3b788
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-16 19:28:26 +0200

    Finish 2.14

--- Osa2/maidentiedot/src/App.js
+++ Osa2/maidentiedot/src/App.js
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const CountryDetails = ({country}) => (
-  <div>
-    <h2>{country.name}</h2>
-    <p>capital {country.capital}</p>
-    <p>population {country.population}</p>
-    <h3>Languages</h3>
-    <ul>{country.languages.map(({ iso639_2, name }) => <li key={iso639_2}>{name}</li>)}</ul>
-    <img src={country.flag} alt="lippu" width="480" />
-  </div>
-);
+const CountryDetails = ({country}) => {
+  const [weatherData, setWeatherData] = useState({});
+  useEffect(() => {
+    axios
+      .get('https://api.apixu.com/v1/current.json?key=d779868b069a4d6fa9e170809191603&q=' + country.capital)
+      .then(({data}) => setWeatherData(data))
+      .catch(reason => console.log(reason));
+  }, []);
+
+  const data = weatherData.current;
+
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h3>Languages</h3>
+      <ul>{country.languages.map(({ iso639_2, name }) => <li key={iso639_2}>{name}</li>)}</ul>
+      <p><img src={country.flag} alt="lippu" width="480" /></p>
+      <h3>Weather in {country.capital}</h3>
+      {data && <div>
+        <p><b>temperature:</b> {Math.round(data.temp_c)} Celsius</p>
+        <p><img src={data.condition.icon} alt="säätila" /></p>
+        <p><b>wind:</b> {data.wind_kph} kph direction {data.wind_dir}</p>
+      </div>}
+    </div>
+  );
+}
 
 const App = () => {
   const [countries, setCountries] = useState([]);
@@ -22,7 +40,7 @@ const App = () => {
       .get('https://restcountries.eu/rest/v2/all')
       .then(({data}) => setCountries(data))
       .catch(reason => console.log(reason));
-  }, [])
+  }, []);
 
   const handleChange = event => {
     setSearch(event.target.value && event.target.value.toLowerCase());
