a06ee147b233f5460674fd4752197328c4c4079e
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-16 09:13:29 +0200

    Finish 1.7

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Heading = ({text}) => <h2>{text}</h2>;
+const Heading = ({ text }) => <h2>{text}</h2>;
 const Button = ({ handleClick, label }) => <button onClick={handleClick}>{label}</button>;
-const Statistics = ({ count, label }) => <p>{label}: {count}</p>;
+const Statistics = (props) => (
+    <div>
+        <p>hyvä: {props.good}</p>
+        <p>neutraali: {props.neutral}</p>
+        <p>huono: {props.bad}</p>
+        <p>yhteensä: {props.count}</p>
+        <p>keskiarvo: {props.average}</p>
+        <p>positiivisia: {props.positive} %</p>
+    </div>);
 
 const App = () => {
     // tallenna napit omaan tilaansa
@@ -13,6 +21,18 @@ const App = () => {
     const addGood = () => setGood(good + 1);
     const addNeutral = () => setNeutral(neutral + 1);
     const addBad = () => setBad(bad + 1);
+    const getStatistics = (good, neutral, bad) => {
+        const count = good + neutral + bad;
+        const total = good * 1 + neutral * 0 + bad * -1;
+        return {
+            good,
+            neutral,
+            bad,
+            count,
+            average: count && total / count,
+            positive: count && Math.round(good / count * 100)
+        };
+    }
 
     return (
         <div>
@@ -21,9 +41,7 @@ const App = () => {
             <Button handleClick={addNeutral} label="neutraali" />
             <Button handleClick={addBad} label="huono" />
             <Heading text="Statistiikka" />
-            <Statistics count={good} label="hyvä" />
-            <Statistics count={neutral} label="neutraali" />
-            <Statistics count={bad} label="huono" />
+            <Statistics {...getStatistics(good, neutral, bad)} />
         </div>
     );
 }
