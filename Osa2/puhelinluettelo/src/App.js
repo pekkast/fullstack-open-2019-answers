8e2ed6e673e318739494a86f7a0b8ff509a35f08
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-16 14:51:18 +0200

    Finish 2.8

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -2,25 +2,30 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '0987654321' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (persons.findIndex(p => p.name === newName) !== -1) {
       return alert(`${newName} on jo luettelossa`);
     }
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, phone: newNumber }));
     setNewName('');
+    setNewNumber('');
   };
-  const handleTextChange = event => setNewName(event.target.value);
+  const handleNameChange = event => setNewName(event.target.value);
+  const handleNumberChange = event => setNewNumber(event.target.value);
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          nimi: <input placeholder="Uusi henkilö" value={newName} onChange={handleTextChange} />
+          nimi: <input placeholder="Uusi henkilö" value={newName} onChange={handleNameChange} /><br />
+          numero: <input placeholder="01234567890" value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">lisää</button>
@@ -28,7 +33,7 @@ const App = () => {
       </form>
       <h2>Numerot</h2>
       <ul>
-        {persons.map(({ name }) => <li key={name}>{name}</li>)}
+        {persons.map(({ name, phone }) => <li key={name}>{name}: {phone}</li>)}
       </ul>
     </div>
   )
