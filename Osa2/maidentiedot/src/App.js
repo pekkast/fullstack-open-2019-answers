90a6eb86a63db6d7f1955b9ef544f75ef99e5022
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-16 19:07:01 +0200

    Finish 2.13

--- Osa2/maidentiedot/src/App.js
+++ Osa2/maidentiedot/src/App.js
@@ -15,6 +15,7 @@ const CountryDetails = ({country}) => (
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [countryCode, setCountryCode] = useState('');
 
   useEffect(() => {
     axios
@@ -23,9 +24,15 @@ const App = () => {
       .catch(reason => console.log(reason));
   }, [])
 
-  const handleChange = event => setSearch(event.target.value && event.target.value.toLowerCase());
+  const handleChange = event => {
+    setSearch(event.target.value && event.target.value.toLowerCase());
+    setCountryCode('');
+  };
+
   const filteredCountries = countries
-    .filter(c => c.name.toLowerCase().indexOf(search) !== -1 || c.nativeName.toLowerCase().indexOf(search) !== -1);
+    .filter(c => countryCode.length > 0 ? countryCode === c.alpha3Code : c.name.toLowerCase().indexOf(search) !== -1 || c.nativeName.toLowerCase().indexOf(search) !== -1);
+
+  const selectCountry = countryCode => () => setCountryCode(countryCode);
 
   return (
     <div>
@@ -33,11 +40,19 @@ const App = () => {
       <div>
         find countries <input value={search} onChange={handleChange} />
       </div>
+      {countryCode.length > 0 && <button onClick={() => setCountryCode('')}>Back to results</button>}
       {filteredCountries.length > 10 && <p>Too many matches.</p>}
       {filteredCountries.length === 1 && <CountryDetails country={filteredCountries[0]} />}
       {filteredCountries.length === 0 && <p>No matches.</p>}
-      {filteredCountries.length <= 10 && filteredCountries.length > 1 &&
-        <ul>{filteredCountries.map(c => <li key={c.alpha3Code}>{c.name}</li>)}</ul>}
+      {filteredCountries.length <= 10 && filteredCountries.length > 1 && <ul>{
+          filteredCountries
+            .map(c =>
+              <li key={c.alpha3Code}>
+                {c.name}
+                <button onClick={selectCountry(c.alpha3Code)}>show</button>
+              </li>
+            )
+      }</ul>}
     </div>
   );
 };
